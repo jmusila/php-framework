37132fbae04a6ba6084721ebb171ecbb0bbe812c
jmusila <jonathanmusila6@gmail.com>
2021-07-02 13:06:22 +0300

    added the core contoller

---
+++ framework/app/libraries/Core.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * App core class
+ * Created URL & loads core controller
+ * URL FORMAT - /controller/method/params
+ */
+class Core
+{
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+}
