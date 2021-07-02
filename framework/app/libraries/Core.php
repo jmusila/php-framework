848609b26dbaa2be247403c9f6c838480d0d9888
jmusila <jonathanmusila6@gmail.com>
2021-07-02 14:40:15 +0300

    added filter url

--- framework/app/libraries/Core.php
+++ framework/app/libraries/Core.php
@@ -10,4 +10,20 @@ class Core
     protected $currentController = 'Pages';
     protected $currentMethod = 'index';
     protected $params = [];
+
+    public function __construct()
+    {
+        $this->getUrl();
+    }
+
+    public function getUrl()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            return $url;
+        }
+    }
 }
--- framework/public/index.php
+++ framework/public/index.php
@@ -1,3 +1,5 @@
 <?php
 
 require_once '../app/boostrap.php';
+
+$core = new Core();
