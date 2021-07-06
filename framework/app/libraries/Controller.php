48fa5afd71d24ce1cbeaab6cfd39cf12e72dd06d
jmusila <jonathanmusila6@gmail.com>
2021-07-06 16:08:19 +0300

    check if view exists

--- framework/app/libraries/Controller.php
+++ framework/app/libraries/Controller.php
@@ -15,7 +15,9 @@ class Controller
     public function view($view, $data = [])
     {
         if(file_exists('../app/views' . $view . '.php')){
-            
+            require_once '../app/views' . $view . '.php';
+        }else{
+            echo 'View does not exist';
         }
     }
 }
