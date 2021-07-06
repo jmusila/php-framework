473b686329089d5a82c7d0765ee763d3d0ee5f1c
jmusila <jonathanmusila6@gmail.com>
2021-07-06 15:51:06 +0300

    added view

--- framework/app/libraries/Controller.php
+++ framework/app/libraries/Controller.php
@@ -3,12 +3,16 @@
 /**
  * Base controller
  */
-class Controller 
+class Controller
 {
     public function model($model)
     {
         require_once '../app/models/' . $model . '.php';
 
         return new $model;
+    }
+
+    public function view($view, $data = [])
+    {
     }
 }
