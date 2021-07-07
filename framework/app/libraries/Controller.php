5e7430044da2d3a7b29a1ef840785c367dc7308f
jmusila <jonathanmusila6@gmail.com>
2021-07-07 14:38:23 +0300

    debug the app

--- framework/app/libraries/Controller.php
+++ framework/app/libraries/Controller.php
@@ -14,8 +14,8 @@ class Controller
 
     public function view($view, $data = [])
     {
-        if(file_exists('../app/views' . $view . '.php')){
-            require_once '../app/views' . $view . '.php';
+        if(file_exists('../app/views/' . $view . '.php')){
+            require_once '../app/views/' . $view . '.php';
         }else{
             echo 'View does not exist';
         }
