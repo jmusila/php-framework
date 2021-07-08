2586943d6f75b70bad1e5b40d89992af7b072cff
jmusila <jonathanmusila6@gmail.com>
2021-07-08 12:01:07 +0300

    added fetch and check methods

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -37,4 +37,32 @@ class Database
             echo $this->error;
         }
     }
+
+    /**
+     * Prepare statements with query
+     */
+    public function query($sql)
+    {
+        $this->statement = $this->dbh->prepare($sql);
+    }
+
+    /**
+     * Bind values
+     */
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+
+                    $type = PDO::PARAM_INT;
+                    
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+        }
+    }
 }
