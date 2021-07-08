ea168d06e9c6564998211a8c146f30e70c39da05
jmusila <jonathanmusila6@gmail.com>
2021-07-08 10:30:06 +0300

    loaded the file

--- app/libraries/DotEnv.php
+++ app/libraries/DotEnv.php
@@ -19,4 +19,19 @@ class DotEnv
         }
         $this->path = $path;
     }
+
+    public function load()
+    {
+        if (!is_readable($this->path)) {
+            throw new RuntimeException(sprintf('%s file is not readbale', $this->path));
+        }
+
+        $lines = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+        }
+    }
 }
